5c2100655a8b48d23fb1e37f300f1b1de9d8c3a0
Liam Tyler <tyler.liam7@gmail.com>
2018-10-31 12:35:52 -0500

    wip: adding robot with spheres rotating

--- examples/example1.cpp
+++ examples/example1.cpp
@@ -7,6 +7,63 @@
 using namespace Progression;
 // using namespace std;
 
+class RotateComponent : public Component {
+public:
+	RotateComponent(GameObject* obj, float speed=0.2) :
+		Component(obj),
+		rotSpeed(speed)
+	{
+	}
+
+	~RotateComponent() = default;
+	void Start() {}
+	void Stop() {}
+
+	void Update() {
+		gameObject->transform.rotation.y += rotSpeed * Time::deltaTime();
+	}
+
+	float rotSpeed;
+};
+
+class RotatingSpheres : public Component {
+public:
+	RotatingSpheres(GameObject* obj, Scene* scene, int num = 8, float radius=2, float speed=0.3) :
+		Component(obj),
+		rotSpeed(speed)
+	{
+		for (int i = 0; i < num; ++i) {
+			GameObject* sphere = new GameObject;
+			sphere->AddComponent<ModelRenderer>(new ModelRenderer(sphere, ResourceManager::GetModel("blueSphere").get()));
+			glm::vec3 relPos(0, 1, 0);
+			float angle = 2 * M_PI * i / num;
+			relPos.x = radius * cos(angle);
+			relPos.z = radius * sin(angle);
+			sphere->transform.position = obj->transform.position + relPos;
+			sphere->transform.scale = glm::vec3(.25);
+
+			spheres_.push_back(sphere);
+			pointLights_.push_back(new Light(Light::Type::POINT, spheres_[i]->transform, glm::vec3(0, 0, 1), 5));
+
+			scene->AddGameObject(sphere);
+			scene->AddLight(pointLights_[i]);
+		}
+	}
+
+	~RotatingSpheres() = default;
+	void Start() {}
+	void Stop() {}
+
+	void Update() {
+	}
+
+	std::vector<GameObject*> spheres_;
+	std::vector<Light*> pointLights_;
+	float rotSpeed;
+
+};
+
+
 float* genKernel(int size, float sigma) {
 	float* kernel = new float[size];
 	int halfSize = size / 2.0f;
@@ -43,10 +100,16 @@ int main(int argc, char* argv[]) {
 	std::cout << "\ncopy shader: " << std::endl;
 	Shader copyShader = Shader(PG_ROOT_DIR "resources/shaders/copy.vert", PG_ROOT_DIR "resources/shaders/copy.frag");
 
-	auto scene = Scene::Load(PG_ROOT_DIR "resources/scenes/glowSphereScene.pgscn");
+	auto scene = Scene::Load(PG_ROOT_DIR "resources/scenes/scene1.pgscn");
 
 	auto camera = scene->GetCamera();
 	camera->AddComponent<UserCameraComponent>(new UserCameraComponent(camera, 3));
+
+	auto robot = scene->GetGameObject("robot");
+	robot->AddComponent< RotatingSpheres>(new RotatingSpheres(robot, scene));
+
+	auto belt = scene->GetGameObject("belt");
+	belt->AddComponent<RotateComponent>(new RotateComponent(belt));
 
 
 	auto skybox = scene->getSkybox();
--- examples/example2.cpp
+++ examples/example2.cpp
@@ -1,58 +1,70 @@
 #include "progression.h"
+#include <iomanip>
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
 
 using namespace Progression;
+// using namespace std;
 
-std::string rootDirectory;
+float* genKernel(int size, float sigma) {
+	float* kernel = new float[size];
+	int halfSize = size / 2.0f;
+	float sum = 0;
+	for (int i = 0; i < size; i++) {
+		kernel[i] = 1.0f / (sqrtf(2 * M_PI) * sigma)  * exp(-0.5 * pow((i - halfSize) / sigma, 2.0));
+		sum += kernel[i];
+	}
+	for (int i = 0; i < size; i++) {
+		kernel[i] /= sum;
+		std::cout << std::setprecision(4) << kernel[i] << " ";
+	}
+	std::cout << std::endl << std::endl;
+
+	return kernel;
+}
 
 int main(int argc, char* argv[]) {
 	srand(time(NULL));
 
-	rootDirectory = "C:/Users/Liam Tyler/Documents/Progression/";
-
-
-	auto& conf = PG::config::Config(rootDirectory + "configs/default.yaml");
+	auto conf = PG::config::Config(PG_ROOT_DIR "configs/default.toml");
+	if (!conf) {
+		std::cout << "could not parse config file" << std::endl;
+		exit(0);
+	}
 
 	PG::EngineInitialize(conf);
 
 	std::cout << "\nbloom combine shader: " << std::endl;
-	Shader bloomCombineShader = Shader(rootDirectory + "resources/shaders/bloomCombine.vert", rootDirectory + "resources/shaders/bloomCombine.frag");
+	Shader bloomCombineShader = Shader(PG_ROOT_DIR "resources/shaders/bloomCombine.vert", PG_ROOT_DIR "resources/shaders/bloomCombine.frag");
 	std::cout << "\nblur shader: " << std::endl;
-	Shader blurShader = Shader(rootDirectory + "resources/shaders/blur.vert", rootDirectory + "resources/shaders/blur.frag");
-	std::cout << "\copy shader: " << std::endl;
-	Shader copyShader = Shader(rootDirectory + "resources/shaders/copy.vert", rootDirectory + "resources/shaders/copy.frag");
+	Shader blurShader = Shader(PG_ROOT_DIR "resources/shaders/blur.vert", PG_ROOT_DIR "resources/shaders/blur.frag");
+	blurShader.AddUniform("kernel");
+	std::cout << "\ncopy shader: " << std::endl;
+	Shader copyShader = Shader(PG_ROOT_DIR "resources/shaders/copy.vert", PG_ROOT_DIR "resources/shaders/copy.frag");
 
-	auto scene = Scene::Load(rootDirectory + "resources/scenes/glowSphereScene.pgscn");
+	auto scene = Scene::Load(PG_ROOT_DIR "resources/scenes/glowSphereScene.pgscn");
 
 	auto camera = scene->GetCamera();
-	camera->AddComponent<UserCameraComponent>(new UserCameraComponent(camera, 15));
+	camera->AddComponent<UserCameraComponent>(new UserCameraComponent(camera, 3));
 
 
 	auto skybox = scene->getSkybox();
 
 
 	GLuint postProcessFBO = graphics::CreateFrameBuffer();
-	GLuint mainBuffer = graphics::Create2DTexture(Window::getWindowSize().x, Window::getWindowSize().y, GL_RGBA, GL_LINEAR, GL_LINEAR);
-	GLuint glowBuffer = graphics::Create2DTexture(Window::getWindowSize().x, Window::getWindowSize().y, GL_RGBA, GL_LINEAR, GL_LINEAR);
+	GLuint mainBuffer = graphics::Create2DTexture(Window::getWindowSize().x, Window::getWindowSize().y, GL_RGBA16F, GL_LINEAR, GL_LINEAR);
+	GLuint glowBuffer = graphics::Create2DTexture(Window::getWindowSize().x, Window::getWindowSize().y, GL_RGBA16F, GL_LINEAR, GL_LINEAR);
 	graphics::AttachColorTexturesToFBO({ mainBuffer, glowBuffer });
 	GLuint rboDepth = graphics::CreateRenderBuffer(Window::getWindowSize().x, Window::getWindowSize().y);
 	graphics::AttachRenderBufferToFBO(rboDepth);
 	graphics::FinalizeFBO();
 
 	GLuint pingPongFBO = graphics::CreateFrameBuffer();
-	GLuint glowBufferBlur = graphics::Create2DTexture(Window::getWindowSize().x, Window::getWindowSize().y, GL_RGBA, GL_LINEAR, GL_LINEAR);
+	GLuint glowBufferBlur = graphics::Create2DTexture(Window::getWindowSize().x, Window::getWindowSize().y, GL_RGBA16F, GL_LINEAR, GL_LINEAR);
 
 	graphics::AttachColorTexturesToFBO({ glowBufferBlur });
 	graphics::FinalizeFBO();
-
-	GLuint halfGlowBuffer = graphics::Create2DTexture(Window::getWindowSize().x / 2, Window::getWindowSize().y / 2, GL_RGBA, GL_LINEAR, GL_LINEAR);
-	GLuint quarterGlowBuffer = graphics::Create2DTexture(Window::getWindowSize().x / 4, Window::getWindowSize().y / 4, GL_RGBA, GL_LINEAR, GL_LINEAR);
-	GLuint eigthGlowBuffer = graphics::Create2DTexture(Window::getWindowSize().x / 8, Window::getWindowSize().y / 8, GL_RGBA, GL_LINEAR, GL_LINEAR);
-	GLuint sixteenthGlowBuffer = graphics::Create2DTexture(Window::getWindowSize().x / 16, Window::getWindowSize().y / 16, GL_RGBA, GL_LINEAR, GL_LINEAR);
-
-	GLuint halfGlowBuffer2 = graphics::Create2DTexture(Window::getWindowSize().x / 2, Window::getWindowSize().y / 2, GL_RGBA, GL_LINEAR, GL_LINEAR);
-	GLuint quarterGlowBuffer2 = graphics::Create2DTexture(Window::getWindowSize().x / 4, Window::getWindowSize().y / 4, GL_RGBA, GL_LINEAR, GL_LINEAR);
-	GLuint eigthGlowBuffer2 = graphics::Create2DTexture(Window::getWindowSize().x / 8, Window::getWindowSize().y / 8, GL_RGBA, GL_LINEAR, GL_LINEAR);
-	GLuint sixteenthGlowBuffer2 = graphics::Create2DTexture(Window::getWindowSize().x / 16, Window::getWindowSize().y / 16, GL_RGBA, GL_LINEAR, GL_LINEAR);
 
 	float quadVerts[] = {
 		-1, 1,
@@ -81,6 +93,25 @@ int main(int argc, char* argv[]) {
 
 	bool blur = true;
 	bool bloom = true;
+	auto kernel5 = genKernel(5, 3);
+	auto kernel11 = genKernel(11, 3);
+	auto kernel21 = genKernel(21, 5);
+	auto kernel41 = genKernel(41, 10);
+	float* kernels[] = { kernel5, kernel11, kernel21, kernel41 };
+	int kernelSizes[] = { 5, 11, 21, 41 };
+
+	const int levels = 4;
+	const int divisorStart = 2;
+	const int divisorMult = 2;
+	GLuint glowBuffers[levels][2];
+	int divisor = divisorStart;
+	for (int i = 0; i < levels; ++i) {
+		glowBuffers[i][0] = graphics::Create2DTexture(Window::getWindowSize().x / divisor, Window::getWindowSize().y / divisor, GL_RGBA16F, GL_LINEAR, GL_LINEAR);
+		glowBuffers[i][1] = graphics::Create2DTexture(Window::getWindowSize().x / divisor, Window::getWindowSize().y / divisor, GL_RGBA16F, GL_LINEAR, GL_LINEAR);
+		divisor *= divisorMult;
+		std::cout << "level = " << i << std::endl;
+	}
+
 	// Game loop
 	while (!PG::EngineShutdown) {
 		PG::Window::StartFrame();
@@ -88,11 +119,6 @@ int main(int argc, char* argv[]) {
 
 		if (PG::Input::GetKeyDown(PG::PG_K_ESC))
 			PG::EngineShutdown = true;
-		if (PG::Input::GetKeyDown(PG::PG_K_G))
-			blur = !blur;
-
-		if (PG::Input::GetKeyDown(PG::PG_K_B))
-			bloom = !bloom;
 
 		scene->Update();
 
@@ -105,78 +131,38 @@ int main(int argc, char* argv[]) {
 		glBindVertexArray(quadVAO);
 		graphics::Bind2DTexture(glowBuffer, copyShader["tex"], 0);
 
-		glViewport(0, 0, Window::getWindowSize().x / 2, Window::getWindowSize().y / 2);
-		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, halfGlowBuffer, 0);
-		glDrawArrays(GL_TRIANGLES, 0, 6);
-
-		glViewport(0, 0, Window::getWindowSize().x / 4, Window::getWindowSize().y / 4);
-		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, quarterGlowBuffer, 0);
-		glDrawArrays(GL_TRIANGLES, 0, 6);
-
-		glViewport(0, 0, Window::getWindowSize().x / 8, Window::getWindowSize().y / 8);
-		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, eigthGlowBuffer, 0);
-		glDrawArrays(GL_TRIANGLES, 0, 6);
-
-		glViewport(0, 0, Window::getWindowSize().x / 16, Window::getWindowSize().y / 16);
-		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, sixteenthGlowBuffer, 0);
-		glDrawArrays(GL_TRIANGLES, 0, 6);
+		divisor = divisorStart;
+		for (int i = 0; i < levels; ++i) {
+			glViewport(0, 0, Window::getWindowSize().x / divisor, Window::getWindowSize().y / divisor);
+			glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, glowBuffers[i][0], 0);
+			glDrawArrays(GL_TRIANGLES, 0, 6);
+			divisor *= divisorMult;
+		}
 
 		// BLUR
 		blurShader.Enable();
 		glBindVertexArray(quadVAO);
+		graphics::ToggleDepthBufferWriting(false);
 
-		if (blur) {
-			// Blur half resolution glowBuffer
-			//---------------------------------------------------------------------------------------------
-			glViewport(0, 0, Window::getWindowSize().x / 2, Window::getWindowSize().y / 2);
-			glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, halfGlowBuffer2, 0);
-			graphics::Bind2DTexture(halfGlowBuffer, blurShader["tex"], 0);
-			glUniform2f(blurShader["offset"], 1.0f / Window::getWindowSize().x * 2.0f, 0);
+		divisor = divisorStart;
+		for (int i = 0; i < levels; ++i) {
+			glUniform1fv(blurShader["kernel"], kernelSizes[i], kernels[i]);
+			glUniform1i(blurShader["halfKernelWidth"], kernelSizes[i] / 2);
+
+			glViewport(0, 0, Window::getWindowSize().x / divisor, Window::getWindowSize().y / divisor);
+			glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, glowBuffers[i][1], 0);
+			graphics::Bind2DTexture(glowBuffers[i][0], blurShader["tex"], 0);
+			glUniform2f(blurShader["offset"], static_cast<float>(divisor) / Window::getWindowSize().x, 0);
 			glDrawArrays(GL_TRIANGLES, 0, 6);
 
-			graphics::Bind2DTexture(halfGlowBuffer2, blurShader["tex"], 0);
-			glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, halfGlowBuffer, 0);
-			glUniform2f(blurShader["offset"], 0, 1.0f / Window::getWindowSize().y * 2.0f);
+			graphics::Bind2DTexture(glowBuffers[i][1], blurShader["tex"], 0);
+			glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, glowBuffers[i][0], 0);
+			glUniform2f(blurShader["offset"], 0, static_cast<float>(divisor) / Window::getWindowSize().y);
 			glDrawArrays(GL_TRIANGLES, 0, 6);
-
-			// Blur quarter resolution glowBuffer
-			//---------------------------------------------------------------------------------------------
-			glViewport(0, 0, Window::getWindowSize().x / 4, Window::getWindowSize().y / 4);
-			glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, quarterGlowBuffer2, 0);
-			graphics::Bind2DTexture(quarterGlowBuffer, blurShader["tex"], 0);
-			glUniform2f(blurShader["offset"], 1.0f / Window::getWindowSize().x * 4.0f, 0);
-			glDrawArrays(GL_TRIANGLES, 0, 6);
-
-			graphics::Bind2DTexture(quarterGlowBuffer2, blurShader["tex"], 0);
-			glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, quarterGlowBuffer, 0);
-			glUniform2f(blurShader["offset"], 0, 1.0f / Window::getWindowSize().y * 4.0f);
-			glDrawArrays(GL_TRIANGLES, 0, 6);
-
-			// Blur eigth resolution glowBuffer
-			//---------------------------------------------------------------------------------------------
-			glViewport(0, 0, Window::getWindowSize().x / 8, Window::getWindowSize().y / 8);
-			glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, eigthGlowBuffer2, 0);
-			graphics::Bind2DTexture(eigthGlowBuffer, blurShader["tex"], 0);
-			glUniform2f(blurShader["offset"], 1.0f / Window::getWindowSize().x * 8.0f, 0);
-			glDrawArrays(GL_TRIANGLES, 0, 6);
-
-			graphics::Bind2DTexture(eigthGlowBuffer2, blurShader["tex"], 0);
-			glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, eigthGlowBuffer, 0);
-			glUniform2f(blurShader["offset"], 0, 1.0f / Window::getWindowSize().y * 8.0f);
-			glDrawArrays(GL_TRIANGLES, 0, 6);
-
-			glViewport(0, 0, Window::getWindowSize().x / 16, Window::getWindowSize().y / 16);
-			glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, sixteenthGlowBuffer2, 0);
-			graphics::Bind2DTexture(sixteenthGlowBuffer, blurShader["tex"], 0);
-			glUniform2f(blurShader["offset"], 1.0f / Window::getWindowSize().x * 16.0f, 0);
-			glDrawArrays(GL_TRIANGLES, 0, 6);
-
-			graphics::Bind2DTexture(sixteenthGlowBuffer2, blurShader["tex"], 0);
-			glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, sixteenthGlowBuffer, 0);
-			glUniform2f(blurShader["offset"], 0, 1.0f / Window::getWindowSize().y * 16.0f);
-			glDrawArrays(GL_TRIANGLES, 0, 6);
+			divisor *= divisorMult;
 		}
 
+		graphics::ToggleDepthBufferWriting(true);
 
 		graphics::BindFrameBuffer(0);
 		glViewport(0, 0, Window::getWindowSize().x, Window::getWindowSize().y);
@@ -191,24 +177,18 @@ int main(int argc, char* argv[]) {
 		glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
 		glBlitFramebuffer(0, 0, Window::getWindowSize().x, Window::getWindowSize().y, 0, 0, Window::getWindowSize().x, Window::getWindowSize().y, GL_DEPTH_BUFFER_BIT, GL_NEAREST);
 
-		if (!bloom) {
-			copyShader.Enable();
-			glBindVertexArray(quadVAO);
-			graphics::Bind2DTexture(mainBuffer, copyShader["tex"], 0);
 
-			glDrawArrays(GL_TRIANGLES, 0, 6);
+		bloomCombineShader.Enable();
+		glUniform1f(bloomCombineShader["bloomIntensity"], 1.0f);
+		glUniform1f(bloomCombineShader["exposure"], 1);
+		glBindVertexArray(quadVAO);
+		graphics::Bind2DTexture(mainBuffer, bloomCombineShader["originalColor"], 0);
+		graphics::Bind2DTexture(glowBuffers[0][0], bloomCombineShader["blur1"], 1);
+		graphics::Bind2DTexture(glowBuffers[1][0], bloomCombineShader["blur2"], 2);
+		graphics::Bind2DTexture(glowBuffers[2][0], bloomCombineShader["blur3"], 3);
+		graphics::Bind2DTexture(glowBuffers[3][0], bloomCombineShader["blur4"], 4);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
 
-		}
-		else {
-			bloomCombineShader.Enable();
-			glBindVertexArray(quadVAO);
-			graphics::Bind2DTexture(mainBuffer, bloomCombineShader["originalColor"], 0);
-			graphics::Bind2DTexture(sixteenthGlowBuffer, bloomCombineShader["fullGlow"], 1);
-			graphics::Bind2DTexture(halfGlowBuffer, bloomCombineShader["halfGlow"], 2);
-			graphics::Bind2DTexture(quarterGlowBuffer, bloomCombineShader["quarterGlow"], 3);
-			graphics::Bind2DTexture(eigthGlowBuffer, bloomCombineShader["eigthGlow"], 4);
-			glDrawArrays(GL_TRIANGLES, 0, 6);
-		}
 
 		graphics::ToggleDepthTesting(true);
 		graphics::ToggleDepthBufferWriting(true);
