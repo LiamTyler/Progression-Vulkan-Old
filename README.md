972cde53ec3475f125ba234c8769dc8df40cb393
Liam Tyler <tyler.liam7@gmail.com>
2018-11-19 00:02:33 -0600

    fix: adding readme

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# OpenGL_Starter
-Just helpful classes / starter code for opengl projects
+# Progression
+My game engine. Currently features forward and tiled deferred rendering, resource management, basic positional audio, and an entity component system
--- progression/core/config.h
+++ progression/core/config.h
@@ -15,24 +15,6 @@ namespace Progression { namespace config {
 		std::shared_ptr<cpptoml::table> operator->() const {
 			return handle_;
 		}
-		/*
-        template <typename T>
-        T as() const {
-            T ret;
-            try {
-                ret = node.as<T>();
-            }
-            catch (std::exception e) {
-                std::cout << "Error converting: " << e.what() << std::endl;
-            }
-            return ret;
-        }
-		*/
-
-        /*template <typename T>
-        Config operator[](const T& key) const {
-            return Config(node[key]);
-        }*/
 
     private:
 		std::shared_ptr<cpptoml::table> handle_;
